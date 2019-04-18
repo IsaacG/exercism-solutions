d7a4a6589a89e843bf824762ddcdb32d05e3f67b
Isaac Good <nuc-git@isaacgood.com>
2019-04-17 21:21:18 -0700

    bash: Hamming distance solution

---
+++ bash/hamming/hamming.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+error () {
+  printf '%s\n' "$*"
+  exit 1
+}
+
+main () {
+  (( $# == 2 )) || error 'Usage: hamming.sh <string1> <string2>'
+  
+  # Regular vars are easier to read when doing fancy parameter expansion.
+  a=$1 b=$2 
+  # Using the a==b||... pattern everywhere in this function. I like consistency.
+  (( ${#a} == ${#b} )) || error 'left and right strands must be of equal length'
+
+  declare -i count
+  for (( i = 0; i < ${#a}; i++ )); do
+    [[ ${a:i:1} == ${b:i:1} ]] || count+=1
+  done
+
+  printf '%d' "$count"
+}
+
+main "$@"
---
+++ bash/hamming/hamming_test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+@test 'empty strands' {
+  run bash hamming.sh '' ''
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 0 ]
+}
+
+@test 'single letter identical strands' {
+  run bash hamming.sh 'A' 'A'
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 0 ]
+}
+
+@test 'single letter different strands' {
+  run bash hamming.sh 'G' 'T'
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 1 ]
+}
+
+@test 'long identical strands' {
+  run bash hamming.sh 'GGACTGAAATCTG' 'GGACTGAAATCTG'
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 0 ]
+}
+
+@test 'long different strands' {
+  run bash hamming.sh 'GGACGGATTCTG' 'AGGACGGATTCT'
+  [ "$status" -eq 0 ]
+  [ "$output" -eq 9 ]
+}
+
+@test 'disallow first strand longer' {
+  run bash hamming.sh 'AATG' 'AAA'
+  [ "$status" -eq 1 ]
+  [ "$output" == 'left and right strands must be of equal length' ]
+}
+
+@test 'disallow second strand longer' {
+  run bash hamming.sh 'ATA' 'AGTG'
+  [ "$status" -eq 1 ]
+  [ "$output" == 'left and right strands must be of equal length' ]
+}
+
+@test "no input" {
+  run bash hamming.sh
+  [ "$status" -eq 1 ]
+  [ "$output" == 'Usage: hamming.sh <string1> <string2>' ]
+}
