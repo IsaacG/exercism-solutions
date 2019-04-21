09c4a3a63f14dc9ab211dd6cd2a58efa4178a7af
Isaac Good <nuc-git@isaacgood.com>
2019-04-20 20:56:39 -0700

    bash: solve Gigasecond

---
+++ bash/gigasecond/gigasecond.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+boolRC () { (( $? == 0 )) && echo true || echo false; }
+
+Gs () {
+  export TZ=UTC
+  birth=$(date +%s -d "$1")
+  date -d "@$((birth + 10**9))" | tr -s ' '
+}
+
+(( $# == 1 )) || exit 1
+Gs "$1"
+
+# vim:ts=2:sw=2:expandtab
