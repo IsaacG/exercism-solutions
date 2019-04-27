0ce7cfaf14a1c6321e1067f4d94abc692cf4d77f
Isaac Good <nuc-git@isaacgood.com>
2019-04-26 21:51:13 -0700

    bash: solve WordCount

---
+++ bash/word-count/word_count.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+word_count () {
+  shopt -s extglob
+  (( $# == 1 )) || exit 1
+  local -A count
+  IFS=', ' read -ra words <<< "${1,,}"  # explicit word splitting
+  IFS=$' \t\n'
+  for w in "${words[@]}"; do
+    w=${w##*([[:punct:]])}
+    w=${w%%*([[:punct:]])}
+    [[ $w ]] || continue
+    [[ -n ${count[$w]} ]] && count[$w]=$(( ${count[$w]} + 1 )) || count[$w]=1
+  done
+
+  for w in "${!count[@]}"; do
+    printf '%s: %d\n' "$w" "${count[$w]}"
+  done
+}
+
+word_count "$@"
+
+# vim:ts=2:sw=2:expandtab
