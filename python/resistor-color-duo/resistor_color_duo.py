165ef626a7fa25a110d710146045aa9cb367646f
Isaac Good <nuc-git@isaacgood.com>
2019-05-03 14:36:50 -0700

    Python: Solve Resistor-color-duo

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,27 @@
+import enum
+
+class Colors(enum.IntEnum):
+  black = 0
+  brown = 1
+  red = 2
+  orange = 3
+  yellow = 4
+  green = 5
+  blue = 6
+  violet = 7
+  grey = 8
+  white = 9
+
+
+def color_code(color):
+  return Colors[color].value
+
+
+def value(colors):
+  val = 0
+  for c in colors:
+    val = val * 10 + color_code(c)
+  return val
+
+
+# vim:ts=2:sw=2:expandtab
