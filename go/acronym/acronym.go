5c3f8e20b0eceed01fc4dde46e134b60263e4f0a
Isaac Good <nuc-git@isaacgood.com>
2019-05-03 13:23:42 -0700

    Go: Solve Acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,19 @@
+// Package acronym solves acronym
+package acronym
+
+import (
+	"strings"
+)
+
+// Abbreviate a string.
+func Abbreviate(s string) string {
+	s = strings.ReplaceAll(s, "-", " ")
+	s = strings.ToUpper(s)
+	var out []byte
+	for _, word := range strings.Split(s, " ") {
+		if len(word) > 0 {
+			out = append(out, word[0])
+		}
+	}
+	return string(out)
+}
