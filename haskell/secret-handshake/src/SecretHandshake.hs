f1445b8970bc11eea416d31c9ab8c2ef2aed06c5
Isaac Good <nuc-git@isaacgood.com>
2019-05-22 01:12:22 -0700

    Haskell: solve SecretHandshake

--- haskell/secret-handshake/src/SecretHandshake.hs
+++ haskell/secret-handshake/src/SecretHandshake.hs
@@ -1,4 +1,19 @@
 module SecretHandshake (handshake) where
 
+import Data.Bits
+
+construct :: Int -> [String]
+construct n =
+  map
+    snd
+    $ filter
+      (testBit n . fst)
+      $ zip [0..] actions
+  where actions = ["wink", "double blink", "close your eyes", "jump"]
+
 handshake :: Int -> [String]
-handshake n = error "You need to implement this function."
+handshake n
+  | testBit n 4 = reverse $ construct n
+  | otherwise   = construct n
+
+-- vim:ts=2:sw=2:expandtab
