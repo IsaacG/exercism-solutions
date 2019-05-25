a6332182381ad72839036db991ef1f02ba8a3e21
Isaac Good <nuc-git@isaacgood.com>
2019-05-24 17:14:00 -0700

    Go: Solve T.go

--- go/t.go
+++ go/t.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"./isbn-verifier"
+	"./luhn"
 	"fmt"
 )
 
 func main() {
-	fmt.Printf("IsValidISBN(3-598-21507-X) %v\n", isbn.IsValidISBN("3-598-21507-X"))
+	fmt.Printf("%r", luhn.Valid("059"))
 }
