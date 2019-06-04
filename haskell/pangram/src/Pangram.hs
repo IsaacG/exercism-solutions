69e2aa4e444cb35b9a2e24035af0a2eadc39f0d1
Isaac Good <nuc-git@isaacgood.com>
2019-06-04 15:32:37 -0700

    Haskell: Solve Pangram/src/Pangram.hs

--- haskell/pangram/src/Pangram.hs
+++ haskell/pangram/src/Pangram.hs
@@ -1,26 +1,20 @@
 module Pangram (isPangram) where
 
 import qualified Data.Char as C
+import qualified Data.Set as S
 
--- Any a in [a]
-iany :: Ord a => a -> [a] -> Bool
-iany c (x:xs)
-  | x == c = True
-  | otherwise = iany c xs
-iany _ [] = False
+foundAll :: S.Set Char -> String -> Bool
+foundAll s (c:cs)
+  | S.size s == 26 = True
+  | otherwise      = foundAll (S.insert c s) cs
+foundAll s []
+  | S.size s == 26 = True
+  | otherwise      = False
 
--- All a in [a]
-iall :: Ord a => a -> [a] -> Bool
-iall c (x:xs)
-  | x /= c = False
-  | otherwise = iall c xs
-iall _ [] = True
 
 isPangram :: String -> Bool
-isPangram text = iall True $ map (\ x -> iany x ctext ) lower
+isPangram text = foundAll S.empty lower_input
   where
-    -- Lowercase and extract letters
-    ctext = map C.toLower $ filter C.isLetter text
-    lower = ['a','b'..'z']
+    lower_input = map C.toLower $ filter C.isLetter text
 
 -- vim:ts=2:sw=2:expandtab
