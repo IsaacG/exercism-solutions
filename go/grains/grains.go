4a10069d35df12cee975ea2758dabcfd52267c67
Isaac Good <nuc-git@isaacgood.com>
2019-06-04 15:32:37 -0700

    Go: Solve Grains/grains.go

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -3,7 +3,6 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Square computes the grains on a given square.
@@ -11,11 +10,10 @@ func Square(s int) (uint64, error) {
 	if s < 1 || s > 64 {
 		return 0, errors.New("invalid square")
 	}
-	return uint64(math.Pow(2, float64(s-1))), nil
+	return 1 << uint(s-1), nil
 }
 
 // Total computes the total grains on a board.
-func Total () uint64 {
-	s, _ := Square(64)
-	return 2 * s - 1
+func Total() uint64 {
+	return 1<<64 - 1
 }
