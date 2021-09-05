6af1421fc7ace8149f9540ac7a32ac05bc2ca946
Isaac Good <nuc-git@isaacgood.com>
2021-09-05 13:35:16 -0700

    Use __getattr__() over specifying all planet functions

--- python/space-age/space_age.py
+++ python/space-age/space_age.py
@@ -1,4 +1,21 @@
+import functools
+
+
+EARTH_SECONDS = 60 * 60 * 24 * 365.25
+PLANET_RATIOS = {
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'earth': 1.0,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132
+}
+
+
 class SpaceAge(object):
+
     def __init__(self, seconds):
         self.seconds = seconds
 
@@ -6,28 +23,9 @@ class SpaceAge(object):
     def years(self):
         return self.seconds / 31557600
 
-    def on_venus(self):
-        return round(self.years / 0.61519726, 2)
+    def __getattr__(self, n):
+        assert n.startswith("on_"), n
+        planet = n.removeprefix("on_")
+        result = round(self.seconds / (EARTH_SECONDS * PLANET_RATIOS[planet]), 2) 
+        return lambda: result
 
-    def on_earth(self):
-        return round(self.years / 1, 2)
-
-    def on_mercury(self):
-        return round(self.years / 0.2408467, 2)
-
-    def on_mars(self):
-        return round(self.years / 1.8808158, 2)
-
-    def on_jupiter(self):
-        return round(self.years / 11.862615, 2)
-
-    def on_saturn(self):
-        return round(self.years / 29.447498, 2)
-
-    def on_uranus(self):
-        return round(self.years / 84.016846, 2)
-
-    def on_neptune(self):
-        return round(self.years / 164.79132, 2)
-
-# vim:ts=2:sw=2:expandtab
