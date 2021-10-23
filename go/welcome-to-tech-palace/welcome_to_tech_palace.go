679fe2695d8a8239b41beaf8d6a31b44053bbdb2
Isaac Good <nuc-git@isaacgood.com>
2021-10-22 18:39:26 -0700

    Solve go/welcome-to-tech-palace

---
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -0,0 +1,22 @@
+// Package techpalace teaches strings.
+package techpalace
+
+import (
+	"fmt"
+	"strings"
+)
+
+// WelcomeMessage returns a welcome message for the customer.
+func WelcomeMessage(customer string) string {
+	return fmt.Sprintf("Welcome to the Tech Palace, %s", strings.ToUpper(customer))
+}
+
+// AddBorder adds a border to a welcome message.
+func AddBorder(welcomeMsg string, numStarsPerLine int) string {
+	return fmt.Sprintf("%s\n%s\n%s", strings.Repeat("*", numStarsPerLine), welcomeMsg, strings.Repeat("*", numStarsPerLine))
+}
+
+// CleanupMessage cleans up an old marketing message.
+func CleanupMessage(oldMsg string) string {
+	return strings.TrimSpace(strings.ReplaceAll(oldMsg, "*", ""))
+}
