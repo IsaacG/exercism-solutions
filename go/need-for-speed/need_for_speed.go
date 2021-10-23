ae3dce5fd0ebbceaa21f961cf6c60bfd584bbae2
Isaac Good <nuc-git@isaacgood.com>
2021-10-22 18:39:26 -0700

    Solve go/need-for-speed

---
+++ go/need-for-speed/need_for_speed.go
@@ -0,0 +1,48 @@
+// Package speed handles car races.
+package speed
+
+// Car encodes a racecar.
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
+
+// Track has track details.
+type Track struct {
+	distance int
+}
+
+// NewCar creates a new remote controlled car with full battery and given specifications.
+func NewCar(speed, batteryDrain int) Car {
+	return Car{
+		battery:      100,
+		batteryDrain: batteryDrain,
+		speed:        speed,
+		distance:     0,
+	}
+}
+
+// NewTrack created a new track
+func NewTrack(distance int) Track {
+	return Track{distance}
+}
+
+// Drive drives the car one time. If there is not enough battery to drive on more time,
+// the car will not move.
+func Drive(car Car) Car {
+	if car.battery < car.batteryDrain {
+		return car
+	}
+	car.distance += car.speed
+	car.battery -= car.batteryDrain
+	return car
+}
+
+// CanFinish checks if a car is able to finish a certain track.
+func CanFinish(car Car, track Track) bool {
+	units := int(car.battery / car.batteryDrain)
+	carRange := car.distance * units
+	return carRange >= track.distance
+}
