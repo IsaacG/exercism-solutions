d62bee8e9a6c1025b4f446e622f9a4b5db0c73ce
Isaac Good <nuc-git@isaacgood.com>
2021-10-22 18:39:26 -0700

    Solve go/booking-up-for-beauty

---
+++ go/booking-up-for-beauty/booking_up_for_beauty.go
@@ -0,0 +1,48 @@
+package booking
+
+import (
+	"fmt"
+	"time"
+)
+
+// Schedule returns a time.Time from a string containing a date
+func Schedule(date string) time.Time {
+	t, err := time.Parse("1/2/2006 15:04:05", date)
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
+// HasPassed returns whether a date has passed
+func HasPassed(date string) bool {
+	t, err := time.Parse("January 2, 2006 15:04:05", date)
+	if err != nil {
+		panic(err)
+	}
+	return t.Before(time.Now())
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon
+func IsAfternoonAppointment(date string) bool {
+	t, err := time.Parse("Monday, January 2, 2006 15:04:05", date)
+	if err != nil {
+		panic(err)
+	}
+	hr, _, _ := t.Clock()
+	return hr >= 12 && hr < 18
+}
+
+// Description returns a formatted string of the appointment time
+func Description(date string) string {
+	t, err := time.Parse("1/2/2006 15:04:05", date)
+	if err != nil {
+		panic(err)
+	}
+	return fmt.Sprintf("You have an appointment on %s, at %s.", t.Format("Monday, January 2, 2006"), t.Format("15:04"))
+}
+
+// AnniversaryDate returns a Time with this year's anniversary
+func AnniversaryDate() time.Time {
+	return time.Date(time.Now().Year(), 9, 15, 0, 0, 0, 0, time.UTC)
+}
