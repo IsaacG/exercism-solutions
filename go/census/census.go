ab4332c46436cb27441824e638bdd612b76b9a8e
Isaac Good <nuc-git@isaacgood.com>
2021-10-22 18:39:26 -0700

    Solve go/census

---
+++ go/census/census.go
@@ -0,0 +1,44 @@
+// Package census simulates a system used to collect census data.
+package census
+
+// Resident represents a resident in this city.
+type Resident struct {
+	Name    string
+	Age     int
+	Address map[string]string
+}
+
+// NewResident registers a new resident in this city.
+func NewResident(name string, age int, address map[string]string) *Resident {
+	return &Resident{name, age, address}
+}
+
+// HasRequiredInfo determines if a given resident has all of the required information.
+func (r *Resident) HasRequiredInfo() bool {
+	if r.Name == "" {
+		return false
+	}
+	if street, exists := r.Address["street"]; !exists || street == "" {
+		return false
+	}
+	return true
+
+}
+
+// Delete deletes a resident's information.
+func (r *Resident) Delete() {
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
+}
+
+// Count counts all residents that have provided the required information.
+func Count(residents []*Resident) int {
+	var count int
+	for _, r := range residents {
+		if r.HasRequiredInfo() {
+			count++
+		}
+	}
+	return count
+}
