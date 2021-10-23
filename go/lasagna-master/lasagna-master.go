7d53fedf8dfc46edc7ef9e0d75669437dcac4006
Isaac Good <nuc-git@isaacgood.com>
2021-10-22 18:39:26 -0700

    Solve go/lasagna-master

---
+++ go/lasagna-master/lasagna-master.go
@@ -0,0 +1,40 @@
+package lasagna
+
+// PreparationTime returns how long it takes to prepare.
+func PreparationTime(layers []string, time int) int {
+	if time == 0 {
+		time = 2
+	}
+	return len(layers) * time
+}
+
+// Quantities returns the quantity of noodles and sauce needed.
+func Quantities(layers []string) (int, float64) {
+	var noodles int
+	var sauce float64
+	for _, contents := range layers {
+		if contents == "noodles" {
+			noodles += 50
+		} else if contents == "sauce" {
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
+
+// AddSecretIngredient adds an ingredient to the list.
+func AddSecretIngredient(friendList, myList []string) []string {
+	want := make([]string, len(myList), len(myList)+1)
+	copy(want, myList)
+	return append(want, friendList[len(friendList)-1])
+}
+
+// ScaleRecipe scales recipe amounts.
+func ScaleRecipe(amounts []float64, portions int) []float64 {
+	out := make([]float64, 0, len(amounts))
+	scale := float64(portions) / 2
+	for _, amount := range amounts {
+		out = append(out, amount*scale)
+	}
+	return out
+}
