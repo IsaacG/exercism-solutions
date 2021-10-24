bb5e364b06b28234dff55bef05dfb38c362306f8
Isaac Good <nuc-git@isaacgood.com>
2021-10-24 10:06:23 -0700

    Tidy up python/dnd_character

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,24 +1,29 @@
+"""D&D Character Generator."""
+
 import random
-
-def modifier(n):
-  return (n - 10) // 2
-
 
 TRAITS = ('strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma')
 
 
+def modifier(stat: int) -> int:
+    """Return the modifier value based on a skill stat."""
+    return (stat - 10) // 2
+
+
 class Character:
+    """D&D character generator."""
 
-  def __init__(self):
-    random.seed()
-    for a in TRAITS:
-      setattr(self, a, self.ability())
+    # For pylint
+    constitution: int
 
-    self.hitpoints = 10 + modifier(self.constitution)
-        
+    def __init__(self):
+        """Initialize the character."""
+        for trait in TRAITS:
+            setattr(self, trait, self.ability())
 
-  def ability(self):
-    dice = sorted([random.randint(1, 6) for _ in range(4)], reverse=True)
-    return sum(dice[0:3])
+        self.hitpoints = 10 + modifier(self.constitution)
 
-# vim:ts=2:sw=2:expandtab
+    def ability(self) -> int:
+        """Roll an ability stat: 4d6 and discard the lowest die."""
+        dice = sorted(random.randint(1, 6) for _ in range(4))
+        return sum(dice[1:])
