efc6325afb63c5247062dab30358700abd8477f6
Isaac Good <nuc-git@isaacgood.com>
2021-10-26 16:12:00 -0700

    Tidy up python/phone-number

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,37 +1,45 @@
-class Phone(object):
-  def __init__(self, num):
-    # Ignore all non-digits.
-    digits = ''.join([i for i in num if i.isdigit()])
+"""Phone number handler."""
 
-    # For an 11-digit with country code, drop the country code.
-    if len(digits) == 11 and digits.startswith('1'):
-      digits = digits[1:]
-    self.number = digits
-    self.validate()
 
-  def validate(self):
-    if len(self.number) != 10:
-      raise ValueError('Invalid number')
+class Phone:
+    """Phone number validator."""
 
-    for invalid in ('0', '1'):
-      for check in (self.area_code, self.exchange):
-        if check.startswith(invalid):
-          raise ValueError('Invalid number')
+    def __init__(self, num: str):
+        """Load and clean a number, validating it."""
+        # Ignore all non-digits.
+        digits = ''.join(i for i in num if i.isdigit())
 
-  @property
-  def area_code(self):
-    return self.number[0:3]
+        # For an 11-digit with country code, drop the country code.
+        if len(digits) == 11 and digits.startswith('1'):
+            digits = digits[1:]
+        self.number = digits
+        self.validate()
 
-  @property
-  def exchange(self):
-    return self.number[3:6]
+    def validate(self) -> None:
+        """Validate the number."""
+        if len(self.number) != 10:
+            raise ValueError('Invalid number')
 
-  @property
-  def subscriber(self):
-    return self.number[6:10]
+        for invalid in ('0', '1'):
+            for check in (self.area_code, self.exchange):
+                if check.startswith(invalid):
+                    raise ValueError('Invalid number')
 
-  def pretty(self):
-    return '(%s) %s-%s' % (self.number[0:3], self.number[3:6], self.number[6:10])
+    @property
+    def area_code(self) -> str:
+        """Return the area code (leading 3 digits)."""
+        return self.number[0:3]
 
-        
-# vim:ts=2:sw=2:expandtab
+    @property
+    def exchange(self) -> str:
+        """Return the exchange (3 digits after the area code)."""
+        return self.number[3:6]
+
+    @property
+    def subscriber(self) -> str:
+        """Return the subscriber (final 4 digits)."""
+        return self.number[6:10]
+
+    def pretty(self) -> str:
+        """Return a pretty string of the number."""
+        return f'({self.area_code}) {self.exchange}-{self.subscriber}'
