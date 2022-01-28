9d0ad7d392bf8cd33f086b58c3ec72c1210a8b0e
Isaac Good <nuc-git@isaacgood.com>
2022-01-28 08:44:02 -0800

    Solve python/largest-series-product/

---
+++ python/largest-series-product/largest_series_product.py
@@ -0,0 +1,20 @@
+"""Largest Series Product."""
+import math
+
+
+def largest_product(series: str, size: int) -> int:
+    """Return the largest series product."""
+    if size == 0:
+        return 1
+    if size > len(series):
+        raise ValueError("span must be smaller than string length")
+    if not series.isdigit():
+        raise ValueError("digits input must only contain digits")
+    if size < 0:
+        raise ValueError("span must be greater than zero")
+
+    numbers = [int(number) for number in series]
+    return max(
+        math.prod(numbers[start:start + size])
+        for start in range(len(series) - size + 1)
+    )
