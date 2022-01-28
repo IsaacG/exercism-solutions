ab4dbdcd890d910f76b55418a0205aad37eb7646
Isaac Good <nuc-git@isaacgood.com>
2022-01-28 08:44:02 -0800

    Solve python/crypto-square/

---
+++ python/crypto-square/crypto_square.py
@@ -0,0 +1,25 @@
+"""Crypto Square."""
+
+import math
+import string
+import textwrap
+
+
+def cipher_text(plain_text: str) -> str:
+    """Encode using a Crypto Square."""
+    # Remove non-alphanum chars.
+    keep = string.ascii_lowercase + string.digits
+    text = "".join(c for c in plain_text.lower() if c in keep)
+
+    # Calculate block size.
+    cols = math.ceil(math.sqrt(len(text))) or 1
+    rows = cols - 1
+    if cols * rows < len(text):
+        rows = cols
+
+    # Space pad as needed.
+    text = text.ljust(cols * rows)
+    # Transpose the block.
+    out = "".join(text[col + row * cols] for col in range(cols) for row in range(rows))
+    # Chunk the result.
+    return " ".join(textwrap.wrap(out, rows or 1, drop_whitespace=False))
