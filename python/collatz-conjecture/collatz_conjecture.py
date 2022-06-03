994b1332b05f38c447d2cb736ec4f97b1766a8d4
Isaac Good <nuc-git@isaacgood.com>
2022-06-03 08:29:13 -0700

    python/collatz-conjecture whitespace formatting, change iterator

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,14 +1,17 @@
-def steps(number):
-  if number < 1:
-    raise ValueError('Only positive numbers are allowed')
+"""Test the Collatz Conjecture."""
+import itertools
 
-  steps = 0
-  while number != 1:
-    if number % 2:
-      number = number * 3 + 1
-    else:
-      number /= 2
-    steps += 1
-  return steps
 
-# vim:ts=2:sw=2:expandtab
+def steps(number: int) -> int:
+    """Return the number of steps required to get a number to 1."""
+    if number < 1:
+        raise ValueError('Only positive numbers are allowed')
+
+    for step in itertools.count():
+        if number == 1:
+            return step
+        if number % 2:
+            number = number * 3 + 1
+        else:
+            number //= 2
+    raise RuntimeError("count should not stop")
