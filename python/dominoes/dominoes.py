9ac0e1b0a84060cc69c9253ccf1eadf2fd19169b
Isaac Good <nuc-git@isaacgood.com>
2022-06-03 08:29:53 -0700

    python/dominoes: add a Domino class for code clarity

--- python/dominoes/dominoes.py
+++ python/dominoes/dominoes.py
@@ -1,7 +1,26 @@
 """Build a chain of dominoes."""
+from __future__ import annotations
 from typing import Generator, Optional
 
-Domino = tuple[int, int]
+
+class Domino(tuple):
+    """Domino tile."""
+
+    def orientations(self) -> list[Domino]:
+        """Return all possible Domino orientations."""
+        return [self, Domino(reversed(self))]
+
+    def can_connect(self, prior: Optional[int]) -> bool:
+        """Return if this Domino can connect to prior."""
+        return prior is None or prior == self[0]
+
+
+def select(dominoes: list[Domino]) -> Generator[tuple[Domino, list[Domino]], None, None]:
+    """Generate tuples of a start Domino and the rest."""
+    for one in set(dominoes):
+        rest = dominoes.copy()
+        rest.remove(one)
+        yield one, rest
 
 
 def build_line(dominoes: list[Domino], prior: Optional[int]) -> Generator[list[Domino], None, None]:
@@ -11,16 +30,13 @@ def build_line(dominoes: list[Domino], prior: Optional[int]) -> Generator[list[D
         return
 
     # Collapse repeat pieces since it doesn't matter which we start with.
-    for start in set(dominoes):
-        rest = dominoes.copy()
-        rest.remove(start)
+    for start, rest in select(dominoes):
         # Try using the piece in both orientations.
-        for domino in (start, (start[1], start[0])):
-            # Check if this piece is valid here on the line.
-            if prior is not None and domino[0] != prior:
+        for domino in start.orientations():
+            if not domino.can_connect(prior):
                 continue
             # Check if the rest of the dominos form a line with this piece.
-            for candidate in build_line(rest, domino[1]):
+            for candidate in build_line(rest, prior=domino[1]):
                 if candidate is not None:
                     yield [domino] + candidate
 
@@ -31,7 +47,7 @@ def can_chain(dominoes: list[Domino]) -> Optional[list[Domino]]:
     if not dominoes:
         return dominoes
     # Check every line and return the first that loops around.
-    for chain in build_line(dominoes, None):
+    for chain in build_line([Domino(d) for d in dominoes], None):
         if chain[0][0] == chain[-1][-1]:
             return chain
     return None
