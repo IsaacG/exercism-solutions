37a003f27d36c762349a61c9c72e22f9aef2add7
Isaac Good <nuc-git@isaacgood.com>
2022-06-18 09:58:42 -0700

    Improve alphametics, rest-api

--- python/alphametics/alphametics.py
+++ python/alphametics/alphametics.py
@@ -9,18 +9,16 @@ def solve(puzzle: str) -> Optional[dict[str, int]]:
     """Return a puzzle solution."""
     left, right = puzzle.split(" == ")
     parts = [p.strip() for p in left.split("+")]
-    letters = {l for l in puzzle if l.isalpha()}
+    words = parts + [right]
+    letters = set().union(*(set(w) for w in words))
     # Try all permutations of digits for the letters.
     for vals in itertools.permutations(string.digits, r=len(letters)):
         mapping = dict(zip(letters, vals))
-        trans = str.maketrans(mapping)
-        t_parts = [p.translate(trans) for p in parts]
-        t_right = right.translate(trans)
+        translation = str.maketrans(mapping)
+        translated = [w.translate(translation) for w in words]
         # All numbers cannot start with a 0.
-        if any(p[0].startswith("0") for p in t_parts):
+        if any(w[0].startswith("0") for w in translated):
             continue
-        if t_right.startswith("0"):
-            continue
-        if sum(int(p) for p in t_parts) == int(t_right):
+        if sum(int(w) for w in translated[:-1]) == int(translated[-1]):
             return {k: int(v) for k, v in mapping.items()}
     return None
--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -3,16 +3,12 @@
 import collections
 import dataclasses
 import json
-from typing import cast, Optional, Union
+from typing import Callable, Optional, Union
 
 
 @dataclasses.dataclass(order=True)
 class User:
-    """One person and how much they owe or are owed.
-
-    Debts this person owes are arbitrarily positive while money owed to this person
-    are negative.
-    """
+    """One person and how much they owe or are owed."""
 
     name: str
     # A collection of debts owed to or from this user.
@@ -20,18 +16,36 @@ class User:
         default_factory=lambda: collections.defaultdict(float)
     )
 
-    def balance(self) -> float:
-        """Return the balance of debts owed to/from this person."""
-        return -sum(self.debts.values())
-
     def asdict(self):
         """Return a formatted dict for this user."""
         return {
             "name": self.name,
             "owes": {name: amount for name, amount in self.debts.items() if amount > 0},
             "owed_by": {name: -amount for name, amount in self.debts.items() if amount < 0},
-            "balance": self.balance(),
+            "balance": -sum(self.debts.values()),
         }
+
+
+class Database(dict):
+    """User database which auto-adds on lookup."""
+
+    def __missing__(self, name) -> User:
+        """Add a User if they are not in the database."""
+        self[name] = User(name)
+        return self[name]
+
+
+def json_encoded(func: Callable[["RestAPI", str, Optional[str]], Union[User, list[User]]]):
+    """Call an API endpoint and return the JSON formatted data."""
+
+    def wrapper(self, url: str, payload: Optional[str] = None) -> str:
+        encoder = json.JSONEncoder(default=lambda u: u.asdict())
+        data = func(self, url, payload)
+        if isinstance(data, list):
+            return encoder.encode({"users": sorted(data)})
+        return encoder.encode(data)
+
+    return wrapper
 
 
 class RestAPI:
@@ -40,49 +54,33 @@ class RestAPI:
     def __init__(self, database=None):
         """Initialize the data."""
         users = database["users"] if database else []
-        self._data = {}
+        self._data = Database()
         for user in users:
-            record = User(user["name"])
-            record.debts.update(user["owes"])
-            # Money owed to this user is tracked as a negative debt.
-            record.debts.update({name: -amount for name, amount in user["owed_by"].items()})
-            self._data[user["name"]] = record
+            _ = self._data[user["name"]]  # Ensure the name is in the database
+            for lender, amount in user["owes"].items():
+                self.record_debt(user["name"], lender, amount)
 
-    def get(self, url: str, payload: Optional[str] = None) -> str:
+    @json_encoded
+    def get(self, url: str, payload: Optional[str] = None) -> list[User]:
         """Return a list of user info, optionally filtered."""
         assert url == "/users"
-        want = json.loads(payload)["users"] if payload else None
-        return self._get(want)
+        names = json.loads(payload)["users"] if payload else self._data.values()
+        return [self._data[name] for name in names]
 
-    def _get(self, want: Optional[list[str]]) -> str:
-        """Return a list of filtered user info."""
-        users = sorted(self._data.values())
-        if want:
-            users = [user for user in users if user.name in want]
-        return json.dumps({"users": [user.asdict() for user in users]})
-
-    def post(self, url: str, payload: Optional[str] = None) -> str:
+    @json_encoded
+    def post(self, url: str, payload: Optional[str] = None) -> Union[User, list[User]]:
         """Handle POST requests: add and iou."""
         assert payload is not None
         parsed = json.loads(payload)
         if url == "/add":
-            return self.add(parsed)
+            return self._data[parsed["user"]]
         if url == "/iou":
-            return self.iou(parsed)
+            borrower, lender, amount = parsed["borrower"], parsed["lender"], parsed["amount"]
+            self.record_debt(borrower, lender, amount)
+            return [self._data[name] for name in (borrower, lender)]
         return NotImplemented
 
-    def add(self, payload: dict[str, str]) -> str:
-        """Add a user to the database."""
-        name = payload["user"]
-        user = User(name)
-        self._data[name] = user
-        return json.dumps(user.asdict())
-
-    def iou(self, payload: dict[str, Union[str, float]]) -> str:
-        """Record a debt."""
-        lender = cast(str, payload["lender"])
-        borrower = cast(str, payload["borrower"])
-        amount = cast(float, payload["amount"])
+    def record_debt(self, borrower: str, lender: str, amount: float) -> None:
+        """Update the database with a new debt."""
         self._data[borrower].debts[lender] += amount
         self._data[lender].debts[borrower] -= amount
-        return self._get([lender, borrower])
