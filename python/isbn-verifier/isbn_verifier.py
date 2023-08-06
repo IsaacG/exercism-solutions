0cd8e8fa75b1c6260804c12fc5bcbbcfa5d9400c
Isaac Good <github@isaacgood.com>
2023-08-06 09:53:40 -0700

    python/isbn-verifier: tidy code

--- python/isbn-verifier/isbn_verifier.py
+++ python/isbn-verifier/isbn_verifier.py
@@ -1,22 +1,20 @@
 from functools import reduce
 
-reduction = lambda count, pair: count + pair[0] * pair[1]
+reduction = lambda count, pair: count + pair[0] * (10 - pair[1])
+
 
 def is_valid(isbn):
-  isbn = isbn.replace('-', '')
-  if len(isbn) != 10:
-    return False
-  nums = list(isbn)
-  # The last character only can be 'X'. Map 'X' => 10
-  if nums[-1] == 'X':
-    nums[-1] = 10
+    isbn = isbn.replace('-', '')
+    if len(isbn) != 10:
+        return False
+    nums = list(isbn)
+    # The last character only can be 'X'. Map 'X' => 10
+    if nums[-1] == 'X':
+        nums[-1] = '10'
+    if not all(num.isdigit() for num in nums):
+        return False
+    ints = (int(num) for num in nums)
 
-  try:
-    count = reduce(reduction, zip(map(int, nums), range(10, 0, -1)), 0)
-  except ValueError:
-    # Invalid input
-    return False
-  return count % 11 == 0
-
-
-# vim:ts=2:sw=2:expandtab
+    count = sum((10 - i) * num for i, num in enumerate(ints))
+    # count = reduce(reduction, zip(ints, range(10)), 0)
+    return count % 11 == 0
