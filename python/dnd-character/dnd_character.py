3a7604effb3b5eafb6c42a092a64b0ebe8bd9861
Isaac Good <github@isaacgood.com>
2023-08-31 12:01:39 -0700

    python/dnd-character: update to pass tests

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,30 +1,21 @@
-"""D&D Character Generator."""
-
 import random
+
+def modifier(n):
+    return (n - 10) // 2
+
 
 TRAITS = ('strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma')
 
 
-def modifier(stat: int) -> int:
-    """Return the modifier value based on a skill stat."""
-    return (stat - 10) // 2
-
-
 class Character:
-    """D&D character generator."""
-
-    # For pylint
-    constitution: int
 
     def __init__(self):
-        """Initialize the character."""
-        for trait in TRAITS:
-            setattr(self, trait, self.ability())
+        random.seed()
+        for a in TRAITS:
+            setattr(self, a, self.ability())
 
         self.hitpoints = 10 + modifier(self.constitution)
 
-    @staticmethod
-    def ability() -> int:
-        """Roll an ability stat: 4d6 and discard the lowest die."""
-        dice = sorted(random.randint(1, 6) for _ in range(4))
-        return sum(dice[1:])
+    def ability(self):
+        dice = sorted([random.randint(1, 6) for _ in range(4)], reverse=True)
+        return sum(dice[0:3])
