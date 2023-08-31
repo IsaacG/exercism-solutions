0595cb7b7344fe0ebcedf314213cf4b435e664d2
Isaac Good <github@isaacgood.com>
2023-08-31 12:03:26 -0700

    python/list-ops: add type annotations with TypeVars

--- python/list-ops/list_ops.py
+++ python/list-ops/list_ops.py
@@ -1,39 +1,41 @@
-def append(list1, list2):
+import typing
+
+S = typing.TypeVar("S")
+T = typing.TypeVar("T")
+
+def append(list1: list[T], list2: list[T]) -> list[T]:
     return list1 + list2
 
 
-def concat(lists):
+def concat(lists: list[list[T]]) -> list[T]:
     result = []
     for l in lists:
         result.extend(l)
     return result
 
 
-def filter(function, a_list):
+def filter(function: typing.Callable[[T], bool], a_list: list[T]) -> list[T]:
     return [i for i in a_list if function(i)]
 
 
-def length(a_list):
-    return len(a_list)
+def length(a_list: list[T]) -> int:
+    return sum(1 for _ in a_list)
 
 
-def map(function, a_list):
+def map(function: typing.Callable[[S], T], a_list: list[S]) -> list[T]:
     return [function(i) for i in a_list]
 
 
-def foldl(function, a_list, initial):
+def foldl(function: typing.Callable[[T, S], T], a_list: list[S], initial: T) -> T:
     result = initial
     for i in a_list:
         result = function(result, i)
     return result
 
 
-def foldr(function, a_list, initial):
-    result = initial
-    for i in reversed(a_list):
-        result = function(i, result)
-    return result
+def foldr(function: typing.Callable[[T, S], T], a_list: list[S], initial: T) -> T:
+    return foldl(function, reverse(a_list), initial)
 
 
-def reverse(a_list):
+def reverse(a_list: list[T]) -> list[T]:
     return list(reversed(a_list))
