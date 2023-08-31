a30aa9de056a9918925035474cdba83860b81e0f
Isaac Good <github@isaacgood.com>
2023-08-31 11:58:28 -0700

    python/bank_account: update to pass tests

--- python/bank-account/bank_account.py
+++ python/bank-account/bank_account.py
@@ -2,14 +2,17 @@ import threading
 
 class BankAccount(object):
   def __init__(self):
+    self.lock = threading.Lock()
     self.balance = None
 
   def get_balance(self):
+    with self.lock:
       if self.balance is None:
         raise ValueError('account not open')
       return self.balance
 
   def open(self):
+    with self.lock:
       if self.balance is not None:
         raise ValueError('account already open')
       self.balance = 0
@@ -17,20 +20,23 @@ class BankAccount(object):
   def deposit(self, amount):
     if amount <= 0:
       raise ValueError('amount must be greater than 0')
-    if self.balance is None:
-      raise ValueError('account not open')
-    self.balance += amount
+    with self.lock:
+      if self.balance is None:
+        raise ValueError('account not open')
+      self.balance += amount
 
   def withdraw(self, amount):
     if amount <= 0:
       raise ValueError('amount must be greater than 0')
-    if self.balance is None:
-      raise ValueError('account not open')
-    if self.balance < amount:
-      raise ValueError('amount must be less than balance')
-    self.balance -= amount
+    with self.lock:
+      if self.balance is None:
+        raise ValueError('account not open')
+      if self.balance < amount:
+        raise ValueError('amount must be less than balance')
+      self.balance -= amount
 
   def close(self):
+    with self.lock:
       if self.balance is None:
         raise ValueError('account not open')
       self.balance = None
