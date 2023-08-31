fcfa7f4423c434d153ffc5d24cd820b4cbe1be4a
Isaac Good <github@isaacgood.com>
2023-08-31 09:47:32 -0700

    python/wordy: use a tokenize approach

--- python/wordy/wordy.py
+++ python/wordy/wordy.py
@@ -3,55 +3,72 @@
 import operator
 import re
 
-
-def raise_unknown() -> None:
-    """Raise an unknown error."""
-    raise ValueError("unknown operation")
+from typing import Callable, Generator
 
 
-# Word problem prefix/suffix.
-PREFIX = "What is "
-SUFFIX = "?"
 # Match a number.
-NUM_RE = re.compile(r"-?[0-9]+")
+NUM_RE = re.compile(r"(-?[0-9]+)[ \?]")
 # Binary (two operands) operations.
 OPS = [
-    (int, re.compile(r"(-?\d+)")),
-    (operator.pow, re.compile(r"(.*) raised to the (-?\d+)th power")),
-    (operator.mul, re.compile(r"(.*) multiplied by (-?\d+)")),
-    (operator.truediv, re.compile(r"(.*) divided by (-?\d+)")),
-    (operator.add, re.compile(r"(.*) plus (-?\d+)")),
-    (operator.sub, re.compile(r"(.*) minus (-?\d+)")),
-    (operator.mod, re.compile(r"(.*) modulus (-?\d+)")),
-    (operator.eq, re.compile(r"(.*) equals (-?\d+)")),
-    (operator.gt, re.compile(r"(.*) is greater than (-?\d+)")),
-    (operator.lt, re.compile(r"(.*) is less than (-?\d+)")),
-    (operator.lt, re.compile(r"(.*) is less than (-?\d+)")),
-    (raise_unknown, re.compile(r"-?\d+ cubed")),
+    (operator.mul, "multiplied by"),
+    (operator.truediv, "divided by"),
+    (operator.add, "plus"),
+    (operator.sub, "minus"),
+    (operator.mod, "modulus"),
+    (operator.gt, "is greater than"),
+    (operator.lt, "is less than"),
+    (operator.lt, "is less than"),
 ]
+
+
+def tokenize(question: str) -> Generator[tuple[Callable[[int, int], int], int], None, None]:
+    """Generator operator, number pairs for a given question.
+
+    Parse a question and yield tuple[operator, int] pairs for the input.
+    The expectation is that we start with a value of 0 and the first operation is a "0 + int".
+    """
+    # Pointer to the start of the next token.
+    position = 0
+    # The end of the question, when we stop parsing.
+    end = len(question)
+
+    def read_int():
+        """Return an int, parsed from the current position."""
+        nonlocal position
+        if match := NUM_RE.match(question[position:]):
+            position += match.end()
+            return int(match.group(1))
+        raise ValueError("syntax error")
+
+    def read_op():
+        """Return an operator, parsed from the current position."""
+        nonlocal position
+        for oper, text in OPS:
+            if question[position:].startswith(text):
+                position += len(text) + 1
+                return oper
+        if NUM_RE.match(question[position:]):
+            raise ValueError("syntax error")
+        raise ValueError("unknown operation")
+
+    # Parse the first two works, "What is ..."
+    if not question[position:].startswith("What "):
+        raise ValueError("unknown operation")
+    position += len("What ")
+    if not question[position:].startswith("is "):
+        raise ValueError("syntax error")
+    position += len("is ")
+
+    # The first operator is an implicit "add".
+    yield operator.add, read_int()
+    # While there is more question, parse operator, int pairs.
+    while position < end:
+        yield read_op(), read_int()
 
 
 def answer(question: str) -> int:
     """Answer a word problem."""
-    if not question.endswith(SUFFIX):
-        raise ValueError("syntax error")
-    if " is " not in question:
-        raise ValueError("syntax error")
-    if not question.startswith(PREFIX):
-        raise ValueError("unknown operation")
-    question = question.removeprefix(PREFIX).removesuffix(SUFFIX)
-    return solve(question)
-
-
-def solve(question: str) -> int:
-    """Solve a problem fragment."""
-    for operation, pattern in OPS:
-        if match := pattern.fullmatch(question):
-            if " " in question:
-                resolver = solve
-            else:
-                # Stop recursing when down to one token.
-                resolver = lambda x: x
-            operands = (resolver(part) for part in match.groups())
-            return operation(*operands)
-    raise ValueError("syntax error")
+    value = 0
+    for oper, num in tokenize(question):
+        value = oper(value, num)
+    return value
