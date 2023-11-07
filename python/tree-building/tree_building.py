bfc2a609429b5e36940e452a91fea4e76c56592a
Isaac Good <github@isaacgood.com>
2023-11-06 18:52:43 -0800

    python/tree-building: switch to dataclasses

--- python/tree-building/tree_building.py
+++ python/tree-building/tree_building.py
@@ -1,44 +1,42 @@
-class Record():
-  def __init__(self, record_id, parent_id):
-    self.record_id = record_id
-    self.parent_id = parent_id
+import dataclasses
 
 
-class Node():
-  def __init__(self, node_id):
-    self.node_id = node_id
-    self.children = []
+@dataclasses.dataclass
+class Record:
+    record_id: int
+    parent_id: int
 
 
-def BuildTree(records):
-  if not records:
-    return None
-
-  nodes = {}
-  records.sort(key=lambda x: x.record_id)
-
-  for record in records:
-    if record.record_id == 0:
-      if record.parent_id != 0:
-        raise ValueError("Node record_id should be smaller than it's parent_id.")
-    elif record.record_id == record.parent_id:
-      raise ValueError('Only root should have equal record and parent id.')
-    if record.record_id < record.parent_id:
-      raise ValueError("Node record_id should be smaller than it's parent_id.")
-
-    nodes[record.record_id] = Node(record.record_id)
-
-  if 0 not in nodes:
-    raise ValueError('Record id is invalid or out of order.')
-  if sorted(nodes.keys()) != list(range(len(nodes))):
-    raise ValueError('Record id is invalid or out of order.')
-
-  for record in records:
-    if record.record_id == 0:
-      continue
-    nodes[record.parent_id].children.append(nodes[record.record_id])
-  
-  return nodes[0]
+@dataclasses.dataclass
+class Node:
+    node_id: int
+    def __post_init__(self) -> None:
+        self.children: list[Node] = []
 
 
-# vim:ts=2:sw=2:expandtab
+def BuildTree(records: list[Record]) -> Node | None:
+    if not records:
+        return None
+
+    nodes = {}
+    records.sort(key=lambda x: x.record_id)
+
+    for record in records:
+        if record.record_id != 0 and record.record_id == record.parent_id:
+            raise ValueError('Only root should have equal record and parent id.')
+        if record.record_id < record.parent_id:
+            raise ValueError("Node parent_id should be smaller than it's record_id.")
+
+        nodes[record.record_id] = Node(record.record_id)
+
+    if 0 not in nodes:
+        raise ValueError('Record id is invalid or out of order.')
+    if sorted(nodes.keys()) != list(range(len(nodes))):
+        raise ValueError('Record id is invalid or out of order.')
+
+    for record in records:
+        if record.record_id == 0:
+            continue
+        nodes[record.parent_id].children.append(nodes[record.record_id])
+    
+    return nodes[0]
