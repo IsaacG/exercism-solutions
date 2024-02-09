ad667ac11075e1bad2ad25fc245bbeafe7dc9239
Isaac Good <github@isaacgood.com>
2024-02-08 23:43:06 -0800

    python/poker: shorten solution

--- python/poker/poker.py
+++ python/poker/poker.py
@@ -3,46 +3,11 @@
 from __future__ import annotations
 
 import collections
-import enum
+import itertools
 
 
 # Face cards. Used to make Jack, Queen, King, Ace to a value.
-FACE = {"J": 11, "Q": 12, "K": 13, "A": 14}
-
-
-class Suit(enum.Enum):
-    """Card suit. This validates the suit value."""
-
-    H = enum.auto()
-    S = enum.auto()
-    C = enum.auto()
-    D = enum.auto()
-
-
-class Category(enum.Enum):
-    """Hand category."""
-
-    STRAIGHT_FLUSH = enum.auto()
-    FOUR_KIND = enum.auto()
-    FULL_HOUSE = enum.auto()
-    FLUSH = enum.auto()
-    STRAIGHT = enum.auto()
-    THREE_KIND = enum.auto()
-    TWO_PAIR = enum.auto()
-    ONE_PAIR = enum.auto()
-    NONE = enum.auto()
-
-    def __lt__(self, other):
-        """Return if this category beats other. Allows sorting."""
-        return self.value < other.value
-
-
-class Outcome(enum.Enum):
-    """Outcome when comparing hands."""
-
-    WIN = enum.auto()
-    LOSE = enum.auto()
-    TIE = enum.auto()
+FACE = {"J": 11, "Q": 12, "K": 13, "A": 14} | {str(i): i for i in range(2, 11)}
 
 
 class Card:
@@ -50,125 +15,82 @@ class Card:
 
     def __init__(self, val: str):
         """Initialize."""
-        self.suit = Suit[val[-1]]
-        pips = val[:-1]
-        if pips in FACE:
-            self.pips = FACE[pips]
-        else:
-            self.pips = int(pips)
-
-    def __lt__(self, other):
-        """Return if self < other. Allows sorting."""
-        return self.pips < other.pips
-
-    @classmethod
-    def from_input(cls, data: str) -> list[Card]:
-        """Return a list of Cards from exercise input."""
-        return [cls(i) for i in data.split()]
+        self.suit = val[-1]
+        self.pips = FACE[val[:-1]]
 
 
 class Hand:
     """A poker hand."""
 
-    def __init__(self, cards: list[Card]):
+    def __init__(self, cards: str):
         """Initialize a Hand."""
-        self.cards = cards
-        assert len(self.cards) == 5, "A hand must contain 5 cards."
-
+        self.original_repr = cards
+        self.cards = [Card(i) for i in cards.split()]
         self.pips = sorted(c.pips for c in self.cards)
-        assert min(self.pips) > 1, "Cards cannot have a value below 2."
-        assert max(self.pips) <= FACE["A"], "Cards cannot have a value greater than Ace."
 
         # Group pip values by count, e.g. [3, 3, 4, 4, 9] => {2: [3, 4], 1: [9]}
         self.counts = collections.defaultdict(list)
         for pips, count in collections.Counter(self.pips).items():
             self.counts[count].append(pips)
+        for counts in self.counts.values():
+            counts.sort(reverse=True)
 
-    @property
-    def flush(self) -> bool:
-        """Return if this hand is a flush, i.e. only one suit."""
-        return len(set(c.suit for c in self.cards)) == 1
-
-    @property
-    def _straight(self) -> bool:
-        """Return if this hand is a regular straight."""
-        paired = zip(self.pips, self.pips[1:])
-        return all(b - a == 1 for a, b in paired)
-
-    @property
-    def _straight_low_ace(self) -> bool:
-        """Return if this hand is a straight using Ace as 1."""
-        if FACE["A"] not in self.pips:
-            return False
-        pips = [1] + self.pips[:-1]
-        paired = zip(pips, pips[1:])
-        return all(b - a == 1 for a, b in paired)
-
-    @property
-    def straight(self) -> bool:
-        """Return if this hand is a straight."""
-        return self._straight or self._straight_low_ace
-
-    @property
-    def category(self) -> Category:
-        """Return the hand category."""
-        if self.flush and self.straight:
-            return Category.STRAIGHT_FLUSH
-        if 4 in self.counts:
-            return Category.FOUR_KIND
-        if 3 in self.counts and 2 in self.counts:
-            return Category.FULL_HOUSE
-        if self.flush:
-            return Category.FLUSH
-        if self.straight:
-            return Category.STRAIGHT
-        if 3 in self.counts:
-            return Category.THREE_KIND
-        if len(self.counts.get(2, [])) == 2:
-            return Category.TWO_PAIR
-        if len(self.counts.get(2, [])) == 1:
-            return Category.ONE_PAIR
-        return Category.NONE
+        self.flush = len(set(c.suit for c in self.cards)) == 1
+        self.low_straight = FACE["A"] in self.pips and self.values_are_straight([1] + self.pips[:-1])
+        self.regular_straight = self.values_are_straight(self.pips)
+        self.straight = self.regular_straight or self.low_straight
 
     @staticmethod
-    def _pips_cmp(self: list[int], other: list[int]) -> Outcome:
-        """Compare a list of pips and determine if one beats the other."""
-        for a, b in zip(sorted(self, reverse=True), sorted(other, reverse=True)):
-            if a > b:
-                return Outcome.WIN
-            if a < b:
-                return Outcome.LOSE
-        return Outcome.TIE
+    def values_are_straight(pips: list[int]) -> bool:
+        return all(b - a == 1 for a, b in zip(pips[:-1], pips[1:]))
 
-    def __cmp__(self, other: Hand) -> Outcome:
-        """Compare hands for an outcome."""
-        # If the categories differ, order by category.
-        s_cat, o_cat = self.category, other.category
-        if s_cat != o_cat:
-            return Outcome.WIN if s_cat < o_cat else Outcome.LOSE
-
-        # Edge case: A2345 loses to 23456.
-        if self.straight and self._straight != other._straight:
-            return Outcome.LOSE if self._straight_low_ace else Outcome.WIN
-
-        # For the same category, compare pips for a tie breaker, in priority of N-of-a-kind.
-        for count in sorted(self.counts, reverse=True):
-            # Ternary state. Pips can indicate win, lose or tie. Tie continues on.
-            cmp = self._pips_cmp(self.counts[count], other.counts[count])
-            if cmp != Outcome.TIE:
-                return cmp
-        return Outcome.TIE
+    @property
+    def category(self) -> int:
+        """Return the hand category."""
+        if self.flush and self.straight:
+            return 8  # STRAIGHT_FLUSH
+        if self.counts[4]:
+            return 7  # FOUR_KIND
+        if self.counts[3] and self.counts[2]:
+            return 6  # FULL_HOUSE
+        if self.flush:
+            return 5  # FLUSH
+        if self.straight:
+            return 4  # STRAIGHT
+        if self.counts[3]:
+            return 3  # THREE_KIND
+        if len(self.counts[2]) == 2:
+            return 2  # TWO_PAIR
+        if len(self.counts[2]) == 1:
+            return 1  # ONE_PAIR
+        return 0  # NONE
 
     def __lt__(self, other: Hand) -> bool:
-        """Return if self beats other. Useful for sorting."""
-        return self.__cmp__(other) == Outcome.WIN
+        """Return if self is lower poitns than other. Useful for sorting."""
+        # If the categories differ, order by category.
+        if self.category != other.category:
+            return self.category < other.category
+
+        # Edge case: A2345 loses to 23456.
+        if self.straight and self.regular_straight != other.regular_straight:
+            return self.low_straight
+
+        # For the same category, compare pips for a tie breaker, in priority of N-of-a-kind.
+        for count in sorted(set(self.counts) | set(other.counts), reverse=True):
+            for a, b in itertools.zip_longest(self.counts[count], other.counts[count], fillvalue=0):
+                if a != b:
+                    return a < b
+        return False
+
+    def __str__(self) -> str:
+        return self.original_repr
 
 
 def best_hands(hands: list[str]) -> list[str]:
     """Return the best poker hands."""
     # Map the input strings to the Hand so the inputs can be reused for returns.
-    the_hands = {i: Hand(Card.from_input(i)) for i in hands}
+    the_hands = [Hand(hand) for hand in hands]
     # Find the best hand. Hands sort from best to worst.
-    best = min(the_hands.values())
+    best = max(the_hands)
     # Return all hands that would tie the best hand.
-    return [k for k, v in the_hands.items() if v.__cmp__(best) == Outcome.TIE]
+    return [str(v) for v in the_hands if not v < best]
